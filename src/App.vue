8f9b69c578316aecf2de3a864a54010732c086d1
Pnj-Aanand <63461053+Pnj-Aanand@users.noreply.github.com>
2020-04-10 20:06:13 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Vue + Vuex Shopping Cart Example</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <ProductList />
+            </div>
+            <div class="col-md-4">
+                <Cart />
+            </div>
+        </div>
+
+        <footer>
+            <small>
+                made by <a href="https://twitter.com/krzysu">Kris Urbas</a>, source code available on <a href="https://github.com/krzysu/vuejs-shopping-cart">github</a>
+            </small>
+        </footer>
+    </div>
+</template>
+
+<script>
+import ProductList from "./components/ProductList.vue";
+import Cart from "./components/Cart.vue";
+
+export default {
+  name: "app",
+  components: {
+    ProductList,
+    Cart
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
